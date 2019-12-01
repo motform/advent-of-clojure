d201c51312bab0699b45f734b06367ed282c2f6b
motform <love.lagerkvist@protonmail.com>
2019-12-01 15:37:33 +0100

    Solve day 1

---
+++ src/advent-of-clojure/one.clj
@@ -0,0 +1,21 @@
+(ns advent-of-clojure.one
+  (:require [clojure.string :as s]
+            [clojure.math.numeric-tower :as m]))
+
+(defn fuel-required [module]
+  (- (m/floor (/ module 3))
+     2))
+
+(defn holistic-fuel [module]
+  (->> module
+      (iterate fuel-required)
+      (take-while pos?)
+      rest
+      (reduce +)))
+
+(def modules (map read-string
+                  (-> "resources/one.dat" slurp (s/split #"\n"))))
+
+(def fuel (->> modules
+              (map holistic-fuel)
+              (reduce +)))
