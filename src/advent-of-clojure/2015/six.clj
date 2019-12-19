fc33ba39fac2cb76fc23441df33d1d81df5905ff
motform <love.lagerkvist@protonmail.com>
2019-12-19 22:39:13 +0100

    Solve day 6 2015 part 2

--- src/advent-of-clojure/2015/six.clj
+++ src/advent-of-clojure/2015/six.clj
@@ -5,13 +5,18 @@
 (def instructions
   (-> "resources/2015/six.dat" slurp str/trimr str/split-lines))
 
+(def instructions-one
+  {"turn on" turn-on
+   "turn off" turn-off
+   "toggle" toggle})
+
 (defn digits [xs]
   (map read-string xs))
 
 (defn lights [[x1 y1 x2 y2]]
-  (into #{} (for [x (range x1 (inc x2))
-                  y (range y1 (inc y2))]
-              [x y])))
+  (for [x (range x1 (inc x2))
+        y (range y1 (inc y2))]
+    [x y]))
 
 (defn turn-on [lights]
   (fn [house]
@@ -21,21 +26,19 @@
   (fn [house]
     (set/difference house lights)))
 
+(defn toggle' [house light]
+  (if (house light) (disj house light) (conj house light)))
+
 (defn toggle [lights]
   (fn [house]
     (reduce toggle' house lights)))
 
-(defn toggle' [house light]
-  (if (house light) (disj house light) (conj house light)))
-
-(defn parse-fn [instruction]
+(defn parse-fn [instruction instructions]
   (-> instruction
       (str/split #"\d")
       first
       str/trim
-      ({"turn on" turn-on
-        "turn off" turn-off
-        "toggle" toggle})))
+      instructions))
 
 (defn parse-range [instruction]
   (let [chars #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \, \-}]
@@ -48,10 +51,37 @@
         lights)))
 
 (defn parse-instruction [instruction]
-  ((parse-fn instruction) (parse-range instruction)))
+  ((parse-fn instruction instructions-one) (into #{} (parse-range instruction))))
 
-(def part-one
+#_(def part-one
+   (->> instructions
+       (map parse-instruction)
+       (reduce #((identity %2) %1) #{})
+       count))
+
+(def lights-matrix
+  (into [] (repeat 1000 (into [] (repeat 1000 0)))))
+
+(def instructions-two
+  {"turn on" inc
+   "turn off" #(-> % dec (max 0))
+   "toggle" #(+ 2 %)})
+
+(defn parse-revised-instruction [instruction]
+  [(parse-fn instruction instructions-two)
+   (parse-range instruction)])
+
+(defn update-point [f]
+  (fn [mat [x y]]
+    (update-in mat [y x] f)))
+
+(defn update-points [mat [f points]]
+  (let [f (update-point f)]
+    (reduce #(f %1 %2) mat points)))
+
+(def part-two
   (->> instructions
-      (map parse-instruction)
-      (reduce #((identity %2) %1) #{})
-      count))
+      (map parse-revised-instruction)
+      (reduce #(update-points %1 %2) lights-matrix)
+      flatten
+      (reduce +)))
