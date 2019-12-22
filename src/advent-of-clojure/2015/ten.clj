095c874f7237c19893824ff13f2b21a9fdcca169
motform <love.lagerkvist@protonmail.com>
2019-12-22 16:24:42 +0100

    Solve day 10 2015

---
+++ src/advent-of-clojure/2015/ten.clj
@@ -0,0 +1,19 @@
+(ns advent-of-clojure.2015.ten)
+
+(def seed 3113322113)
+
+(defn digits [x]
+  (map #(Character/getNumericValue %) (str x)))
+
+(defn chain [digits]
+  (let [occurances (partition-by identity digits)]
+    (interleave (map count occurances) (map first occurances))))
+
+(defn look-and-say [seed]
+  (iterate chain seed))
+
+(def part-one
+  (->> seed digits look-and-say (drop 1) (take 40) last count))
+
+(def part-two
+  (->> seed digits look-and-say (drop 1) (take 50) last count))
