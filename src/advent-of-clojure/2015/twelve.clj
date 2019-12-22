24cbc983799b33190442e753f7b589b9ef3cd0df
motform <love.lagerkvist@protonmail.com>
2019-12-22 21:54:19 +0100

    Solve day 12 2015

---
+++ src/advent-of-clojure/2015/twelve.clj
@@ -0,0 +1,16 @@
+(ns advent-of-clojure.2015.twelve
+  (:require [clojure.data.json :as json]))
+
+(def books
+  (-> "resources/2015/twelve.json" slurp json/read-str))
+
+(defn red? [node]
+  (or (vector? node)
+      (and (map? node)
+           (not (.contains (vals node) "red")))))
+
+(def part-one
+  (->> books (tree-seq coll? identity) (filter number?) (reduce +)))
+
+(def part-two
+  (->> books (tree-seq red? identity) (filter number?) (reduce +)))
