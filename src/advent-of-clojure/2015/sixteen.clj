1dc213a7a1692170a451acca2419871ab309d7bf
motform <love.lagerkvist@protonmail.com>
2019-12-28 13:19:49 +0100

    Solve part 2 day 16 2015

--- src/advent-of-clojure/2015/sixteen.clj
+++ src/advent-of-clojure/2015/sixteen.clj
@@ -1,6 +1,6 @@
 (ns advent-of-clojure.2015.sixteen
   (:require [clojure.string :as str]
-            [clojure.data :refer [diff]]))
+            [clojure.data :refer [diff]])) ; rare core sighting!
 
 (defn parse-sue [sue]
   (->> sue (re-seq #"\d+|\w+") rest (map #(%1 %2) (cycle [read-string identity]))))
@@ -20,3 +20,18 @@
       (sort-by #(count (nth % 2)))
       last
       (#(get (second %) "nth"))))
+
+(def op {"cats" < "trees" < "pommeranians" > "goldfish" >})
+
+(defn funcify-true-sue [true-sue]
+  (reduce-kv (fn [m k v]
+               (assoc m k #((get op k =) v %)))
+             {} true-sue))
+
+(def part-two
+  (let [true-sue (assoc (funcify-true-sue the-one-true-sue) "nth" identity)]
+    (->> sues
+        (map #(reduce-kv (fn [xs k v] (conj xs ((true-sue k) v))) [] %))
+        (map #(remove false? %))
+        (sort-by count)
+        reverse)))
