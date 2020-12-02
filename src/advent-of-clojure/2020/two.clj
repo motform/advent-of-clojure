bcdbe67450e58c8e7d03b82f2503a3ead02670b9
motform <love.lagerkvist@protonmail.com>
2020-12-02 11:23:39 +0100

    lispify in-range?

--- src/advent-of-clojure/2020/two.clj
+++ src/advent-of-clojure/2020/two.clj
@@ -2,13 +2,13 @@
   (:require [clojure.string :as str]))
 
 (defn parse-password [[x y c _ password]]
-  [(Integer/parseInt x) (Integer/parseInt y) (.charAt c 0) password])
+  [(Integer. x) (Integer. y) (.charAt c 0) password])
 
 (def passwords
   (->> "resources/2020/two.dat" slurp str/split-lines (map (comp parse-password #(str/split % #"\W")))))
 
 (defn in-range? [x min max]
-  (when x (and (>= x min) (<= x max))))
+  (when x (<= min x max)))
 
 (defn valid-password? [[min max c password]]
   (-> password frequencies (get c) (in-range? min max)))
