5a23859c87d61fd0320667096e7b882635df61d0
motform <love.lagerkvist@protonmail.com>
2020-12-03 11:42:15 +0100

    tidy up solution

--- src/advent-of-clojure/2020/three.clj
+++ src/advent-of-clojure/2020/three.clj
@@ -4,16 +4,12 @@
 (def geography (->> "resources/2020/three.dat" slurp str/split-lines))
 
 (defn traverse [geography x-step y-step]
-  (let [width (count (first geography))]
-    (->> (map (fn [x y]
-                (nth (geography y) (mod x width)))
-              (range x-step 9999 x-step)
-              (range y-step (count geography) y-step))
-         (filter #{\#})
-         count)))
+  (->> (map #(nth (geography %2) %1)
+            (iterate #(mod (+ x-step %) (count (first geography))) x-step)
+            (range y-step (count geography) y-step))
+       (filter #{\#})
+       count))
 
 (def one (traverse geography 3 1))
 
-(def variations [[1 1] [3 1] [5 1] [7 1] [1 2]])
-
-(def two (apply * (map #(apply traverse geography %) variations)))
+(def two (apply * (map #(apply traverse geography %) [[1 1] [3 1] [5 1] [7 1] [1 2]])))
