866299380153624e9667f86766feb9afd3d6eba4
motform <love.lagerkvist@protonmail.com>
2020-12-07 18:19:30 +0100

    solve day 7 2020

---
+++ src/advent-of-clojure/2020/seven.clj
@@ -0,0 +1,36 @@
+(ns advent-of-clojure.2020.seven
+  (:require [clojure.string :as str]))
+
+(declare contains-bag)
+
+(def input (->> "resources/2020/seven.dat" slurp str/split-lines (map #(str/split % #" contain no other bags.| contain |, "))))
+
+(defn parse-rule [[container & contained]]
+  (conj (map (partial re-seq #"\d|\w+\s\w+") contained)
+        (re-find #"\w+\s\w+" container)))
+
+(def rules
+  (reduce (fn [m [c & cs]]
+            (assoc m c (reduce (fn [m [n c'd]] (assoc m c'd (Integer. n))) {} cs)))
+          {} (map parse-rule input)))
+
+(defn contains-bag? [bag container]
+  (or (get-in rules [container bag])
+      (pos? (contains-bag bag (keys (get rules container))))))
+
+(defn contains-bag [bag containers]
+  (reduce
+   (fn [n container]
+     (if (contains-bag? bag container) (inc n) n))
+   0 containers))
+
+(def one (contains-bag "shiny gold" (keys rules)))
+
+(defn count-bags [container]
+  (reduce-kv
+   (fn [acc bag n]
+     (+ acc n (* n (count-bags (get rules bag)))))
+   0
+   container))
+
+(def two (count-bags (get rules "shiny gold")))
