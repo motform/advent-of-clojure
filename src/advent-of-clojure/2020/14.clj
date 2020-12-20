95e70ca0d3120c0b6106250f2ac04b5aea193476
motform <love.lagerkvist@protonmail.com>
2020-12-20 23:20:04 +0100

    solve day 14 2020 part 2

--- src/advent-of-clojure/2020/14.clj
+++ src/advent-of-clojure/2020/14.clj
@@ -3,11 +3,6 @@
 
 (def init (-> "resources/2020/14.dat" slurp str/split-lines))
 
-(def t (str/split-lines "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0"))
-
 (defn parse-op [op]
   (if (str/includes? op "mem")
     (re-seq #"\d+" op)
@@ -19,28 +14,56 @@ mem[8] = 0"))
 (defn long->binary-str [x]
   (Long/toBinaryString (Long. x)))
 
-(defn mask [m x]
-  (case m (\X x) x m))
-
-(defn apply-mask [xs ms]
-  (apply str (map mask ms xs)))
-
 (defn binary-string->long [b]
   (read-string (str "2r" b)))
+
+(defn apply-mask [xs ms]
+  (apply str (map (fn [m x] (case m (\X x) x m)) ms xs)))
 
 (defn mask-val [m x]
   (-> x long->binary-str pad-binary-str (apply-mask m) binary-string->long))
 
-(defn update-mem [mem addr val]
-  (assoc mem addr val))
-
-(defn boot [init]
+(defn boot1 [init]
   (reduce
    (fn [{:keys [mask] :as cpu} [x y]] ; x = addr||mask, y = val
      (if-not y
        (assoc cpu :mask x)
-       (update cpu :mem update-mem x (mask-val mask y))))
+       (assoc-in cpu [:mem x] (mask-val mask y))))
    {:mask nil :mem {}}
    init))
 
-(def one (->> init (map parse-op) boot :mem vals (apply +)))
+(def one (->> init (map parse-op) boot1 :mem vals (apply +)))
+
+(defn indexes-of [s val]
+  (remove nil? (map-indexed (fn [i c] (when (= val c) i)) s)))
+
+(defn permute-mask
+  ([mask]
+   (let [indxs (indexes-of mask \X)
+         mask  (apply vector mask)]
+     (flatten (permute-mask mask indxs))))
+  ([mask [i & is]]
+   (if-not i
+     (apply str mask)
+     [(permute-mask (assoc mask i \a) is)
+      (permute-mask (assoc mask i \b) is)])))
+
+(defn apply-mask2 [xs ms]
+  (apply str (map (fn [m x] (case m \0 x (\1 \a) \1 \b \0)) ms xs)))
+
+(defn mask-addr [masks addr]
+  (map (comp binary-string->long #(apply-mask2 addr %)) masks))
+
+(defn masked-addrs [masks addr val]
+  (zipmap (mask-addr masks (pad-binary-str (long->binary-str addr))) (repeat val)))
+
+(defn boot2 [init]
+  (reduce
+   (fn [{:keys [masks] :as cpu} [x y]] ; x = addr||mask, y = val
+     (if y
+       (update cpu :mem #(merge % (masked-addrs masks x (Integer. y))))
+       (assoc cpu :masks (permute-mask x))))
+   {:masks [] :mem {}}
+   init))
+
+(def two (->> init (map parse-op) boot2 :mem vals (apply +)))
