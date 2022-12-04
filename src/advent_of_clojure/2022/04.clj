aac1ab47dee6d0d73b05c938c0d357bddc5c4ee5
motform <love.lagerkvist@protonmail.com>
2022-12-04 16:19:41 +0100

    Clean up 04 2022

--- src/advent_of_clojure/2022/04.clj
+++ src/advent_of_clojure/2022/04.clj
@@ -1,6 +1,5 @@
 (ns advent-of-clojure.2022.04
-  (:require [clojure.set :as set]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
 (def parse-digits
   (comp (partial mapv parse-long)
@@ -9,15 +8,14 @@
 (def input (->> "resources/2022/04.dat" slurp str/split-lines (mapv parse-digits)))
 
 (defn in-bounds? [[x1 y1 x2 y2]]
-  (or (and (<= x2 x1) (>= y2 y1))
-      (and (<= x1 x2) (>= y1 y2))))
+  (or (<= x1 x2 y2 y1)
+      (<= x2 x1 y1 y2)))
 
 (def part-one (->> input (filter in-bounds?) count))
 
 (defn overlaps? [[x1 y1 x2 y2]]
-  (seq (set/intersection
-    (set (range x1 (inc y1)))
-    (set (range x2 (inc y2))))))
+  (or (<= x1 y2 y1)
+      (<= x2 y1 y2)))
 
 (def part-two (->> input (filter overlaps?) count))
 
