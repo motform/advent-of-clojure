0218dcfd70d45352c01f0ca193dacc5000299a0a
motform <love.lagerkvist@protonmail.com>
2022-12-06 08:35:29 +0100

    Clean up solution

--- src/advent_of_clojure/2022/06.clj
+++ src/advent_of_clojure/2022/06.clj
@@ -6,13 +6,15 @@
 (defn first-marker [n buffer]
   (->> buffer
        (partition n 1)
-       (map (partial apply str))
-       (filter #(= n (-> % distinct count)))
-       first))
+       (filter (partial apply distinct?))
+       first
+       (apply str)))
 
 (defn preamble-lenght [n buffer]
-  (->> buffer (first-marker n) (str/index-of buffer) (+ n)))
+  (->> buffer
+       (first-marker n)
+       (str/index-of buffer)
+       (+ n)))
 
 (def part-one (preamble-lenght 4 input))
-
 (def part-two (preamble-lenght 14 input))
